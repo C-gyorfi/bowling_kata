e5231fc2c289e0df562fd9d835a977f3a3351b98
csabagyorfi <csaba@madetech.com>
2019-02-18 19:19:37 +0000

    refactoring

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Bowling
   def initialize
     @total_score = 0
@@ -8,25 +10,26 @@ class Bowling
     @frames.each_with_index do |frame, i|
       extra_points = 0
 
-      if has_previous_frame?(i) && frame_is_strike?(i-1)
+      if has_previous_frame?(i) && frame_is_strike?(i - 1)
         if frame_is_strike?(i)
-          extra_points = frame[0] + next_frame_for(i)[0] unless is_bonus_frame?(i)
+          extra_points = frame[0] + next_frame_for(i)[0] unless bonus_frame?(i - 1)
         else
           extra_points = frame.sum
         end
-      elsif has_previous_frame?(i) && frame_is_spare?(i-1)
+      elsif has_previous_frame?(i) && frame_is_spare?(i - 1)
         extra_points = frame[0]
       end
 
-      @total_score += frame.sum + extra_points
+      @total_score += extra_points
+
+      @total_score += frame.sum unless bonus_frame?(i)
     end
 
-    substrakt_bonus_frames
     @total_score
   end
 
   def throw(*frame)
-      @frames << [frame[0], frame[1]].compact
+    @frames << [frame[0], frame[1]].compact
   end
 
   def frames
@@ -35,11 +38,12 @@ class Bowling
 
   private
 
+  def bonus_frame?(i)
+    i > 9
+  end
+
   def frame_is_strike?(i)
-    if @frames[i].sum == 10 && @frames[i].length == 1
-      return true
-    end
-    false
+    @frames[i][0] == 10
   end
 
   def frame_is_spare?(i)
@@ -50,17 +54,9 @@ class Bowling
     frame_index > 0
   end
 
-  def is_bonus_frame?(i)
-    i > 10
-  end
-
-  def substrakt_bonus_frames
-    @total_score -= @frames[10].sum unless @frames[10].nil?
-    @total_score -= @frames[11].sum unless @frames[11].nil?
-  end
-
   def next_frame_for(i)
-    return [0] if @frames[i+1].nil?
-    @frames[i+1]
+    return [0] if @frames[i + 1].nil?
+
+    @frames[i + 1]
   end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,129 +1,99 @@
+# frozen_string_literal: true
 require 'bowling'
 
 describe Bowling do
   let(:game) { Bowling.new }
   describe '#score' do
+    context 'when the game starts' do
+      it 'scores 0' do
+        expect(game.score).to eq(0)
+      end
 
-  context 'when the game starts' do
-   it 'scores 0' do
-     expect(game.score).to eq(0)
-   end
+      it 'has frames []' do
+        expect(game.frames).to eq([])
+      end
+    end
 
-   it 'has frames []' do
-     expect(game.frames).to eq([])
-   end
- end
+    context 'when player scores without previous spare or strike' do
+      it 'scores the sum of the pins that he put down' do
+        pins_down = rand(11)
+        game.throw(pins_down, 0)
 
-  context 'when player scores without previous spare or strike' do
-   it 'scores the sum of the pins that he put down' do
-     pins_down = rand(11)
-     game.throw(pins_down, 0)
+        expect(game.score).to eq(pins_down)
+        expect(game.frames).to eq([[pins_down, 0]])
+      end
+    end
 
-     expect(game.score).to eq(pins_down)
-     expect(game.frames).to eq([[pins_down, 0]])
-   end
- end
+    context 'when player throws twice' do
+      it 'scores the sum of pins' do
+        first_throw_hit = rand(6)
+        second_throw_hit = rand(6)
 
- context 'when player throws twice' do
-   it 'scores the sum of pins' do
-     first_throw_hit = rand(6)
-     second_throw_hit = rand(6)
+        game.throw(first_throw_hit, second_throw_hit)
 
-     game.throw(first_throw_hit, second_throw_hit)
+        expect(game.score).to eq(first_throw_hit + second_throw_hit)
+      end
+    end
 
-     expect(game.score).to eq(first_throw_hit + second_throw_hit)
-   end
- end
-
- context 'when player throws a spare' do
-   it 'scores the sum of the frame plus doubles of the first throw in next frame' do
-     game.throw(9, 1)
-     game.throw(3, 0)
-     expect(game.score).to eq(16)
-   end
- end
-
-#this could be the next logical step but its green...
- # context 'when player throws multiple spares through the game' do
- #   xit 'scores the sum of each frame plus double of the first throw in next frame for each spare' do
- #     game.throw(5, 5)
- #     game.throw(3, 3)
- #     game.throw(5, 5)
- #     game.throw(3, 3)
- #     expect(game.score).to eq(38)
- #   end
- # end
-
- context 'when player throws all spares through the game, there is a bonus throw after the last frame' do
-   it 'scores the sum of the frame plus the first throw in next frame for each spare' do
-    10.times { game.throw(5,5) }
-    game.throw(5)
-    expect(game.score).to eq(150)
-   end
-
-   it 'scores the sum of the frame plus the first throw in next frame for each spare' do
-    first_throw_hit = rand(10)
-    second_throw_hit = 10 - first_throw_hit
-    bonus_throw_hit = rand(10)
-    expected_score = (first_throw_hit * 19) + (second_throw_hit * 10) + bonus_throw_hit
-    10.times { game.throw(first_throw_hit,second_throw_hit) }
-    game.throw(bonus_throw_hit)
-    expect(game.score).to eq(expected_score)
-   end
- end
-
- context 'when player throws a strike' do
-   it 'scores the sum of the frame plus the first throw in next frame for each spare' do
-    game.throw(10)
-    game.throw(1,2)
-    expect(game.score).to eq(16)
-   end
- end
-
- context 'when player throws all strikes through the game, there is a bonus throw after the last frame' do
-   it 'scores the sum of the frame plus the next two throws for each strike' do
-    10.times { game.throw(10) }
-    game.throw(10)
-    game.throw(10)
-    expect(game.score).to eq(300)
-   end
-
-   it '..' do
-    game.throw(8,2)
-    game.throw(7,2)
-    game.throw(10)
-    game.throw(10)
-    game.throw(6,4)
-    game.throw(6,4)
-    game.throw(10)
-    game.throw(10)
-    game.throw(9,1)
-    game.throw(3,7)
-    game.throw(10)
-    expect(game.score).to eq(190)
-   end
- end
+    context 'when player throws a spare' do
+      it 'scores the sum of the frame plus doubles of the first throw in next frame' do
+        game.throw(9, 1)
+        game.throw(3, 0)
+        expect(game.score).to eq(16)
+      end
+    end
 
 
+    context 'when player throws all spares through the game, there is a bonus throw after the last frame' do
+      it 'scores the sum of the frame plus the first throw in next frame for each spare' do
+        10.times { game.throw(5, 5) }
+        game.throw(5)
+        expect(game.score).to eq(150)
+      end
 
-  #
-  #  it 'can score 17 if scored a spare 5,5 and 3,1,0,0...' do
-  #    2.times { game.bowl(5) }
-  #    game.bowl(3)
-  #    game.bowl(1)
-  #    16.times { game.bowl(0) }
-  #    expect(game.score).to eq(17)
-  #  end
-  #
-  #  it 'can score multiple spares 5,5,3,1,5,5,3,1,0,0...' do
-  #    2.times { game.bowl(5) }
-  #    game.bowl(3)
-  #    game.bowl(1)
-  #    2.times { game.bowl(5) }
-  #    game.bowl(3)
-  #    game.bowl(1)
-  #    12.times { game.bowl(0) }
-  #    expect(game.score).to eq(34)
-  #  end
+      it 'scores the sum of the frame plus the first throw in next frame for each spare' do
+        first_throw_hit = rand(10)
+        second_throw_hit = 10 - first_throw_hit
+        bonus_throw_hit = rand(10)
+        expected_score = (first_throw_hit * 19) + (second_throw_hit * 10) + bonus_throw_hit
+
+        10.times { game.throw(first_throw_hit, second_throw_hit) }
+        game.throw(bonus_throw_hit)
+
+        expect(game.score).to eq(expected_score)
+      end
+    end
+
+    context 'when player throws a strike' do
+      it 'scores the sum of the frame plus the first throw in next frame for each spare' do
+        game.throw(10)
+        game.throw(1, 2)
+        expect(game.score).to eq(16)
+      end
+    end
+
+    context 'when player throws all strikes through the game, there is a bonus throw after the last frame' do
+      it 'scores the sum of the frame plus the next two throws for each strike' do
+        10.times { game.throw(10) }
+        game.throw(10)
+        game.throw(10)
+        expect(game.score).to eq(300)
+      end
+
+      it '..' do
+        game.throw(8, 2)
+        game.throw(7, 2)
+        game.throw(10)
+        game.throw(10)
+        game.throw(6, 4)
+        game.throw(6, 4)
+        game.throw(10)
+        game.throw(10)
+        game.throw(9, 1)
+        game.throw(3, 7)
+        game.throw(10)
+        expect(game.score).to eq(190)
+      end
+    end
   end
 end
